a465dac1964096a69d335172d9ef91b19aa8d65e
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-01-10 09:34:33 -0500

    mvp

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -27,6 +27,9 @@ router.get("/", (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   Product.findOne({
+    where: {
+      id: req.params.id,
+    },
     include: [
       {
         model: Category,
@@ -58,15 +61,15 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
+      // if (req.body.tagIds.length) {
+      //   const productTagIdArr = req.body.tagIds.map((tag_id) => {
+      //     return {
+      //       product_id: product.id,
+      //       tag_id,
+      //     };
+      //   });
+      //   return ProductTag.bulkCreate(productTagIdArr);
+      // }
       // if no product tags, just respond
       res.status(200).json(product);
     })
